a385409b0e85d09a3a0728e6fb89b39b0a54cff2
David Maiato <142182549+dmaiato@users.noreply.github.com>
2025-11-05 17:16:28 -0300

    26. Remove Duplicates from Sorted Array

---
+++ 26.rs
@@ -0,0 +1,19 @@
+impl Solution {
+    pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
+        
+        if nums.is_empty() {
+            return 0;
+        }
+
+        let mut unique_index: usize = 1;
+
+        for i in 1..nums.len() {
+            if nums[i] != nums[unique_index - 1] {
+                nums[unique_index] = nums[i];
+
+                unique_index += 1;
+            }
+        }
+        return unique_index as i32;
+    }
+}
