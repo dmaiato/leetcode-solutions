c228acd6a795fe56950e6d8121687043b11c75b1
David Maiato <142182549+dmaiato@users.noreply.github.com>
2025-11-23 17:42:24 -0300

    35. Search Insert Position

---
+++ go/35.go
@@ -0,0 +1,9 @@
+func searchInsert(nums []int, target int) int {
+    
+    for i := 0; i < len(nums); i++ {
+        if nums[i] >= target {
+            return i
+        }
+    }
+    return len(nums)
+}
